4589b5fa18724dad29e864cf0b902540a222cebe
Dynasty <kydiamond33@gmail.com>
2022-10-24 11:35:55 -0700

    fixed the how to use button in the howitworks component, it was not opening the modal

--- src/components/HowItWorks.tsx
+++ src/components/HowItWorks.tsx
@@ -1,15 +1,14 @@
 import { useState } from 'react';
 import questionMarkIcon from '../assets/questionMark.svg';
-// import { toggleManualModal } from '../utils/func';
+import { toggleManualModal } from '../utils/func';
 
 interface Props {}
 
 const HowItWorks = ({}: Props) => {
-	const [showManualModal, setshowManualModal] = useState<boolean>(false);
 	return (
 		<div
 			className='flex items-center gap-1 cursor-pointer'
-			onClick={() => setshowManualModal(true)}
+			onClick={() => toggleManualModal(true)}
 		>
 			<span className='text-grayTxt text-sm'>How to use</span>{' '}
 			<img
